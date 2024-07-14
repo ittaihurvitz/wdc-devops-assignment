c5303b94cb56d86a948ff585b1b470130258cd74
Ittai Hurvitz <ittaihu@gmail.com>
2024-07-14 16:17:52 +0300

    fixed pod pending problem by changing instance type to medium

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -161,7 +161,7 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.medium"]
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_node,
